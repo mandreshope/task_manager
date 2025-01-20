13ab5f43b55b2e439aa2e78b47e4102b38f09b2f
mandreshope <mandreshope@gmail.com>
2025-01-20 16:27:24 +0300

    Initial Commit

---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Mandresy on 20/01/2025.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
